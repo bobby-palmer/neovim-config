3546273cd73733be4a8555727ce875f24273d8af
bobby-palmer <112954547+bobby-palmer@users.noreply.github.com>
2023-05-28 07:24:16 -0400

    added autocomplete and snippets

--- lua/bobby/plugins-setup.lua
+++ lua/bobby/plugins-setup.lua
@@ -64,6 +64,16 @@ return require('packer').startup(function(use)
   -- configuring lsp servers
   use("neovim/nvim-lspconfig") -- easily configure language servers
 
+  -- autocompletion
+  use("hrsh7th/nvim-cmp") -- completion plugin
+  use("hrsh7th/cmp-buffer") -- source for text in buffer
+  use("hrsh7th/cmp-path") -- source for file system paths
+
+  -- snippets
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
   if packer_bootstrap then
     require('packer').sync()
   end
